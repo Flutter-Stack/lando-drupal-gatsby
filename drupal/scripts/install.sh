2bad511f0fbe48df9f364dd6edd89ab87bc87b8c
Suresh Kumara <kumara.suresh25@gmail.com>
2022-03-20 18:08:01 +0100

    running bsic unamy and garsby

--- drupal/scripts/install.sh
+++ drupal/scripts/install.sh
@@ -4,7 +4,8 @@
 rm -rf gatsby/.cache
 
 cd drupal
-drush --yes site:install demo_umami --db-url=mysql://drupal8:drupal8@database:3306/drupal8 --account-pass=admin --site-name='Drupal-Gatsby'
-drush --yes pm:enable jsonapi gatsby
-drush --yes pm:uninstall contact
-drush --yes config:import --partial --source=../config
+vendor/bin/drush --yes site:install demo_umami --db-url=mysql://drupal9:drupal9@database:3306/drupal9 --account-pass=admin --site-name='Drupal-Gatsby'
+vendor/bin/drush --yes pm:enable jsonapi gatsby devel devel_generate
+vendor/bin/drush --yes pm:uninstall contact
+vendor/bin/drush --yes config:import --partial --source=../config
+vendor/bin/drush  devel-generate-content 50
